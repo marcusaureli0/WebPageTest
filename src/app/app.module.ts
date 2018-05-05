2e306fdeebb643ca8f77e62864ef14025345f0f4
Marcus <marcus_aurelioo@hotmail.com>
2018-05-05 11:59:40 -0300

    starting the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ContentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
